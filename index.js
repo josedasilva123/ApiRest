ac31aa3580e15766abc4937eb62c43019e5d9653
Alex Conder <alex@kenzie.com.br>
2022-06-28 13:32:39 -0300

    fix: CORS

--- index.js
+++ index.js
@@ -16,17 +16,6 @@ app.use(
     })
 )
 
-//Resolvendo erros de CORS
-
-/*
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");      
-    next();   
-});
-*/
-
 app.use(express.json());
 
 
